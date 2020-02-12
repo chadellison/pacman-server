f751fa4d07aa96a72ad074b05addb409221541d6
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-12 16:05:20 -0600

    adjust time sync

--- app/models/game_time.rb
+++ app/models/game_time.rb
@@ -2,7 +2,6 @@ class GameTime
   def self.get_time_data(sent_time)
     server_time = Time.now.to_f * 1000
     difference = server_time - sent_time
-    difference *= -1 if difference < 0
     {serverTime: server_time, difference: difference}
   end
 end
