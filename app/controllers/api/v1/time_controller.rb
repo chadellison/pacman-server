137e14c812384c6b910e118d01d9818e2aa338f5
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-16 16:36:18 -0600

    update time logic

--- app/controllers/api/v1/time_controller.rb
+++ app/controllers/api/v1/time_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V1
     class TimeController < ApplicationController
       def index
-        render json: GameTime.get_time_data(time_params[:start_time].to_f)
+        render json: GameTime.get_time_data(time_params[:sent_time].to_f)
       end
 
       private
 
       def time_params
-        params.permit(:start_time)
+        params.permit(:sent_time)
       end
     end
   end
--- app/models/game_time.rb
+++ app/models/game_time.rb
@@ -1,6 +1,6 @@
 class GameTime
-  def self.get_time_data(start_time)
-    difference = (Time.now.to_f * 1000) - Time.at(start_time).utc.to_f
-    {difference: difference}
+  def self.get_time_data(sent_time)
+    difference = (Time.now.to_f * 1000) - Time.at(sent_time).utc.to_f
+    { difference: difference }
   end
 end
