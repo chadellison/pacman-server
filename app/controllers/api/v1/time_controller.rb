f1666b5b80b72bfa8b92b4022683a17345c26f3f
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-16 08:36:53 -0600

    only calculate server difference froom start time

--- app/controllers/api/v1/time_controller.rb
+++ app/controllers/api/v1/time_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V1
     class TimeController < ApplicationController
       def index
-        render json: GameTime.get_time_data(time_params)
+        render json: GameTime.get_time_data(time_params[:start_time].to_f)
       end
 
       private
 
       def time_params
-        params.permit(:sent_time, :start_time)
+        params.permit(:start_time)
       end
     end
   end
--- app/models/game_time.rb
+++ app/models/game_time.rb
@@ -1,8 +1,6 @@
 class GameTime
-  def self.get_time_data(time_params)
-    client_difference = time_params[:sent_time].to_f - Time.at(time_params[:start_time].to_f / 1000).to_f
-    server_difference = (Time.now.to_f * 1000) - Time.at(time_params[:start_time].to_f / 1000).to_f
-    difference = client_difference - server_difference
+  def self.get_time_data(start_time)
+    difference = (Time.now.to_f * 1000) - Time.at(start_time).utc.to_f
     {difference: difference}
   end
 end
