6903a2dc37e35d4c229880c08fec13d455ecc6ff
Chad Ellison <chad.ellison0123@gmail.com>
2020-04-11 12:53:13 -0500

    do not add ai player to players on server

--- app/models/player.rb
+++ app/models/player.rb
@@ -101,8 +101,7 @@ class Player
   end
 
   def self.deploy_supply_ship
-    players = get_players
-    ai_player = {
+    {
       id: 'ai',
       location: {x: 1800, y: 1125},
       angle: 1,
@@ -117,8 +116,5 @@ class Player
       gameEvent: 'supplyShip',
       updatedAt: (Time.now.to_f * 1000).round
     }
-    players['ai'] = ai_player
-    REDIS.set('players', players.to_json)
-    ai_player
   end
 end
