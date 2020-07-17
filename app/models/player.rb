3517104d14374dffef09bc58c8320d74999d6682
Chad Ellison <chad.ellison0123@gmail.com>
2020-07-17 00:04:59 -0500

    update player update events

--- app/models/player.rb
+++ app/models/player.rb
@@ -43,41 +43,42 @@ class Player
     def update_player(player_data)
       players = Player.get_players
       player = players[player_data['index'].to_i]
-      case player_data['gameEvent']
-      when 'up'
-        player['accelerate'] = true
-        player['trajectory'] = player_data['trajectory']
-      when 'upStop'
-        player['accelerate'] = false
-        player['trajectory'] = player_data['trajectory']
-        player['lastAccelerationTime'] = (Time.now.to_f * 1000).round
-      when 'left'
-        player['rotate'] = 'left'
-      when 'leftStop'
-        player['rotate'] = 'none'
-      when 'right'
-        player['rotate'] = 'right'
-      when 'rightStop'
-        player['rotate'] = 'none'
-      when 'shop'
-        player['armor'] = player_data['armor']
-        player['damage'] = player_data['damage']
-        player['weaponIndex'] = player_data['weaponIndex']
-        player['velocity'] = player_data['velocity']
-        player['shipIndex'] = player_data['shipIndex']
-        player['maxHitpoints'] = player_data['maxHitpoints']
-      end
-
-      player['gameEvent'] = player_data['gameEvent']
-      player['kills'] = player_data['kills']
-      player['location'] = player_data['location']
-      player['angle'] = player_data['angle']
-      player['hitpoints'] = player_data['hitpoints']
-      player['gold'] = player_data['gold']
-      player['score'] = player_data['score']
-      player['items'] = player_data['items']
-      player['effects'] = player_data['effects']
-      player['killedBy'] = player_data['killedBy']
+      # case player_data['gameEvent']
+      # when 'up'
+      #   player['accelerate'] = true
+      #   player['trajectory'] = player_data['trajectory']
+      # when 'upStop'
+      #   player['accelerate'] = false
+      #   player['trajectory'] = player_data['trajectory']
+      #   # player['lastAccelerationTime'] = (Time.now.to_f * 1000).round
+      # when 'left'
+      #   player['rotate'] = 'left'
+      # when 'leftStop'
+      #   player['rotate'] = 'none'
+      # when 'right'
+      #   player['rotate'] = 'right'
+      # when 'rightStop'
+      #   player['rotate'] = 'none'
+      # when 'shop'
+      #   player['armor'] = player_data['armor']
+      #   player['damage'] = player_data['damage']
+      #   player['weaponIndex'] = player_data['weaponIndex']
+      #   player['velocity'] = player_data['velocity']
+      #   player['shipIndex'] = player_data['shipIndex']
+      #   player['maxHitpoints'] = player_data['maxHitpoints']
+      # end
+      #
+      # player['gameEvent'] = player_data['gameEvent']
+      # player['kills'] = player_data['kills']
+      # player['location'] = player_data['location']
+      # player['angle'] = player_data['angle']
+      # player['hitpoints'] = player_data['hitpoints']
+      # player['gold'] = player_data['gold']
+      # player['score'] = player_data['score']
+      # player['items'] = player_data['items']
+      # player['effects'] = player_data['effects']
+      # player['killedBy'] = player_data['killedBy']
+      player = player_data
       player['updatedAt'] = (Time.now.to_f * 1000).round
       players[player['index']] = player
       REDIS.set('players', players.to_json)
